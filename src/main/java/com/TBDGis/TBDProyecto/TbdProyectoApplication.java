93940a46e31cb4c0fca7277c38f683244b493463
Rodrigo <rodrigo.arcos@usach.cl>
2020-12-27 00:22:57 -0300

    Inicio proyecto

---
+++ src/main/java/com/TBDGis/TBDProyecto/TbdProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.TBDGis.TBDProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbdProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TBDGis/TBDProyecto/TbdProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TBDGis.TBDProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
